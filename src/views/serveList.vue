644b96d2aecf13ad108d39afbc3f7c687fbb47d6
黄好杰 <13612989029@163.com>
2020-06-12 09:28:03 +0800

    保存一下

--- src/views/serveList.vue
+++ src/views/serveList.vue
@@ -1,78 +1,88 @@
 <template>
-  <div class="news">
-    <!-- 服务号消息列表 -->
-    <div class="serve-list">
-      <div class="news-list-title flex">
-        <div class="title-main flex">
-          <bui-image src="/image/test.jpg" radius='10' width="40px" height="40px"></bui-image>
-          <text class="c3">泊云客</text>
+    <div ref="wrap">
+        <!-- 服务号消息列表 -->
+        <div class="serve-list">
+            <div class="news-list-title flex">
+                <div class=" flex">
+                    <bui-image class="mr20" src="/image/new.png" radius='17' width="34px" height="34px"></bui-image>
+                    <text class="c0 f28 fw5 ml20">泊云客</text>
+                </div>
+                <text class="f26 c9" @click="serveMoreEvent">全部</text>
+            </div>
+            <div class="serve-list-content">
+                <div class="content-item flex" v-for="(item,index) in serveList" :key='index' @click='serveListItemEvent(index)'>
+                    <text class="f28 c0 w550 fw4 lines1">{{item}}</text>
+                    <text class="f24 c153 fw4">03-04</text>
+                </div>
+            </div>
         </div>
-        <text class="f26 c9" @click="serveMoreEvent">更多</text>
-      </div>
-      <div class="serve-list-content">
-        <div class="content-item flex serve-item" v-for="(item,index) in serveList" :key='index'
-          @click='serveListItemEvent(index)'>
-          <text class="f30 c6 w550 lines1">个信需求接受or拒接,解密产品组和项目1111111111111111</text>
-          <text class="f22 c6">03-04</text>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        serveList: ['', '', '', '', '']
-      }
-    },
-    methods: {
-      // 服务号更多
-      serveMoreEvent() {
-        this.$alert('服务更多')
-      },
-      // 服务号列表
-      serveListItemEvent() {
-        this.$alert('服务号列表')
-      }
+    var lapi = require('linkapi');
+    export default {
+        data() {
+            return {
+                serveList: ['品高云入围Forrest报告，妥妥的！', '魏明慧：公安行业部的行业云探索与实践', '纵享丝滑，聆客也可以！', '吴盈佳：政企行业的探索与实践']
+            }
+        },
+        methods: {
+            // 服务号更多
+            serveMoreEvent() {
+                this.$alert('服务更多')
+            },
+            // 服务号列表
+            serveListItemEvent() {
+                this.$alert('服务号列表')
+            },
+            broadcastWidgetHeight() {
+                let _params = this.$getPageParams();
+                setTimeout(() => {
+                    dom.getComponentRect(this.$refs.wrap, (ret) => {
+                        var channel = new BroadcastChannel('WidgetsMessage')
+                        channel.postMessage({
+                            widgetHeight: ret.size.height,
+                            id: _params.id
+                        });
+                        channel.close();
+                    });
+                }, 100)
+            }
+        },
+        mounted() {
+            lapi.getLanguage((lang) => {
+                this.$alert(lang);
+            })
+            this.broadcastWidgetHeight()
+        }
     }
-  }
 </script>
 
 <style lang="css" src="../css/common.css"></style>
 <style>
-  .news {
-    flex: 1;
-    background-color: #f2f2f2;
-  }
+    .title-main {
+        width: 150px;
+    }
 
-  .title-main {
-    width: 150px;
-  }
+    .content-item {
+        padding: 0 24px;
+        height: 79px;
+    }
 
-  .content-item {
-    padding: 20px;
-    border-top: 1px solid #eee;
-  }
+    .news-list-title {
+        padding: 19px 23px 19px 25px;
+        height: 86px;
+    }
 
-  .news-list-title {
-    padding: 0 20px;
-    height: 86px;
-  }
+    .news-list {
+        background-color: #fff;
+        border-radius: 10px;
+        margin-bottom: 20px;
+    }
 
-  .news-list {
-    background-color: #fff;
-    border-radius: 10px;
-    margin-bottom: 20px;
-  }
-
-  .serve-list {
-    background-color: #fff;
-    border-radius: 10px;
-  }
-
-  .serve-item {
-    height: 100px;
-  }
+    .serve-list {
+        background-color: #fff;
+        border-radius: 10px;
+    }
 </style>
